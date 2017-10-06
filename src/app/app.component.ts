cff0924b870a1cef8d26757104d8b2ec454d84a6
Jonathan Sledge <jonathan.sledge@schencksc.com>
2017-10-06 12:40:46 -0500

    updated references for newest version of material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { DataSource } from '@angular/cdk/collections';
+import { MatSort } from '@angular/material';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/observable/merge';
+import 'rxjs/add/operator/map';
+
 
 @Component({
   selector: 'app-root',
