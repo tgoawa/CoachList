45f47408368f6489614d24e55e1b0b3eda9a2b5e
Jonathan Sledge <jonathan.sledge@schencksc.com>
2017-12-07 08:23:31 -0600

    updated how coach list was generated and applied conditional displaying for coach name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,27 +24,9 @@ export class AppComponent implements OnInit {
     this.tmService.getActiveTeamMembers()
       .subscribe(data => {
         this.activeTeamMembers = data;
-        this.mapCoachToTeamMember(this.activeTeamMembers);
       }, error => {
         console.error(error);
       });
-  }
-
-  private mapCoachToTeamMember(data: TeamMember[]) {
-    for (let x = 0; x < data.length; x++) {
-      const currentTeamMember: TeamMember = data[x];
-      for (let y = 0; y < data.length; y++) {
-        if (data[y].TeamMemberId === currentTeamMember.CoachId) {
-          currentTeamMember.CoachLastFirstName = data[y].LastFirstName;
-          this.coachList.push(currentTeamMember);
-          break;
-        } else if (currentTeamMember.CoachId === 0) {
-          currentTeamMember.CoachLastFirstName = '';
-          this.coachList.push(currentTeamMember);
-          break;
-        }
-      }
-    }
   }
 
 }
