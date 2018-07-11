a5b874f39ba1af14ec07899d206cba65373ee9ee
Jonathan Sledge <jonathan.sledge@schencksc.com>
2018-07-11 11:05:15 -0500

    implement proper sort behavior

--- src/app/team-member-list/team-member-list.component.ts
+++ src/app/team-member-list/team-member-list.component.ts
@@ -1,24 +1,28 @@
-import { Component, Input, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild, AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { TeamMember, ExportData } from '../team-member';
-import { MatPaginator, MatTableDataSource } from '@angular/material';
+import { MatPaginator, MatTableDataSource, MatSort } from '@angular/material';
 import { Angular2Csv } from 'angular2-csv/Angular2-csv';
 import { CoachList } from '../coachList';
 
 @Component({
   selector: 'app-team-member-list',
   templateUrl: './team-member-list.component.html',
-  styleUrls: ['./team-member-list.component.css']
+  styleUrls: ['./team-member-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TeamMemberListComponent implements AfterViewInit {
   @Input() coachList: TeamMember[];
   @ViewChild(MatPaginator) paginator: MatPaginator;
-  displayedColumns = ['lastName', 'firstName', 'title', 'category', 'location', 'businessUnit', 'coach'];
+  @ViewChild(MatSort) sort: MatSort;
+  displayedColumns = ['LastName', 'FirstName', 'JobCodeDescription', 'JobCategory', 'Location', 'BusinessUnit', 'CoachLastName'];
   dataSource = new MatTableDataSource();
-  constructor() { }
+  constructor(private cd: ChangeDetectorRef) { }
 
   ngAfterViewInit() {
     this.dataSource.data = this.coachList;
     this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+    this.cd.detectChanges();
   }
 
   // exportToCSV() {
